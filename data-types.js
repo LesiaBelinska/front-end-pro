014286a623788e8bb48b11f680dba296089a6e60
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-09-05 09:03:30 +0300

    Create data-types.js

---
+++ data-types.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let numberValue = 5;
+let stringValue = "hello";
+let bigIntValue = 224343545456565632323134343534666767567434n;
+let booleanValue = true;
+let undefinedValue;
+let nullValue = null;
+let symbolValue = Symbol('symbol');
+
+console.log(typeof numberValue);
+console.log(typeof stringValue);
+console.log(typeof bigIntValue);
+console.log(typeof booleanValue);
+console.log(typeof undefinedValue);
+console.log(typeof nullValue);
+console.log(typeof symbolValue);
