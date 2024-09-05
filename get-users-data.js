0e49e1a328a28d3af155e2c8ff6d0113409f740e
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-09-05 21:44:53 +0300

    Create get-users-data.js

---
+++ get-users-data.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let userName = prompt("Enter your name").trim();
+let userSurname = prompt("Enter your surname").trim();
+let userCity = prompt("Enter your city").trim();
+
+console.log(
+  `user name is: ${userName}, user surname is:  ${userSurname}, user city is: ${userCity}`
+);
