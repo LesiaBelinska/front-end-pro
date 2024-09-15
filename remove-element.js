09341c50e189707457298f392941c4db97ac0c44
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-09-15 17:04:12 +0300

    Update remove-element.js

--- remove-element.js
+++ remove-element.js
@@ -1,11 +1,27 @@
 "use strict";
 
-let myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let myArray = [1, 3, 4, 6, 2, 5, 7];
 
-const removeElement = function (array, index, amount = 1) {
+const removeElement = function (array, item) {
+  const index = myArray.indexOf(item);
+
+  if (index !== -1) {
+    array.splice(index, 1);
+  }
+
+  return array;
+};
+
+console.log(removeElement(myArray, 3));
+
+// second variant
+
+let mySecondArray = [1, 3, 4, 6, 2, 5, 7];
+
+const deleteElement = function (array, index, amount = 1) {
   array.splice(index, amount);
 
   return array;
 };
 
-console.log(removeElement(myArray, 2));
+console.log(deleteElement(mySecondArray, 2, 3));
