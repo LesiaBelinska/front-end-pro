b95ff99fb03b26a0e60a2a9de682afe0738be82d
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-10-08 09:14:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 "use strict"
+
+function createTable() {
+  const table = document.createElement("table");
+
+  for (let i = 1; i <= 10; i++) {
+    const row = document.createElement("tr");
+
+    for (let j = 1; j <= 10; j++) {
+      const cell = document.createElement("td");
+      cell.textContent = i * j; 
+      row.appendChild(cell); 
+    }
+    table.appendChild(row);
+  }
+
+  return table;
+}
+
+const tableContainer = document.querySelector(".table-wrapper");
+
+tableContainer?.appendChild(createTable());
