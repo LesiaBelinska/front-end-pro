1d47108239a7ee555133010478e8df3f816eeca8
Lesia Belinska <lesya.belinskaya@appflame.com>
2024-10-16 13:59:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-"use strict"
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+    let link;
+
+    const getLinkButton = document.querySelector("#get-link-button");
+    const redirectButton = document.querySelector("#redirect-button");
+
+    if (getLinkButton) {
+        getLinkButton.addEventListener("click", () => {
+            link = prompt("Enter a link");
+        });
+    }
+
+    if (redirectButton) {
+        redirectButton.addEventListener("click", () => {
+            if (link) {
+                window.location.replace(link);
+            }
+        });
+    }
+});
