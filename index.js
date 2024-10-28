aa99b6e48e613a514e30f5da9aadf4e4faece147
Lesia Belinska <lesya.belinskaya@appflame.com>
2024-10-28 17:34:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 "use strict";
+
+class BankAccount {
+  constructor(balance = 0) {
+    this.balance = balance;
+    };
+
+  getBalance() {
+    return this.balance;
+    };
+
+  deposit(depositSum) {
+    if (depositSum <= 0) {
+      console.log("Deposit amount must be greater than zero.");
+      return;
+    }
+
+    this.balance = this.balance + depositSum;
+    };
+
+  withdraw(withdrawSum) {
+    if (withdrawSum <= 0) {
+      console.log("Withdrawal amount must be greater than zero.");
+      return;
+    }
+
+    if (withdrawSum > this.balance) {
+      console.log("Insufficient funds for this withdrawal.");
+      return;
+    }
+
+    this.balance = this.balance - withdrawSum;
+    };
+}
+
+const account1 = new BankAccount(1000);
+
+console.log("Your balance: ", account1.getBalance());
+
+account1.deposit(500);
+
+console.log("Your balance: ", account1.getBalance());
+
+account1.withdraw(200);
+
+console.log("Your balance: ", account1.getBalance());
