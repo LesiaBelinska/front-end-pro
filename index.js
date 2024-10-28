d7122f45e5c766b9786a75398896cced22dafeea
Lesia Belinska <lesya.belinskaya@appflame.com>
2024-10-28 16:49:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-"use strict"
+"use strict";
+
+class Calculator {
+  add(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+    };
+
+  subtract(firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
+    };
+
+  multiply(firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+    };
+
+  divide(firstNumber, secondNumber) {
+    return firstNumber / secondNumber;
+    };
+}
+
+const calc = new Calculator();
+
+console.log(calc.add(5, 3));
+
+console.log(calc.subtract(10, 4));
+
+console.log(calc.multiply(3, 6));
+
+console.log(calc.divide(8, 2));
