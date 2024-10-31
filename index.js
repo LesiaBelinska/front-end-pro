eebed128ca6b9e8169d136dcb618a1f5399b7f07
Lesia Belinska <lesya.belinskaya@appflame.com>
2024-10-31 10:47:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 "use strict";
+
+let timeLeft = 85;
+const MINUTE_IN_SECONDS = 60;
+
+const startCountdown = () => {
+    const timerElement = document.querySelector("#timer");
+
+    const updateTimer = () => {
+        let minutes = Math.floor(timeLeft / MINUTE_IN_SECONDS);
+        let seconds = timeLeft % MINUTE_IN_SECONDS;
+
+        if (seconds < 10) seconds = "0" + seconds;
+        if (minutes < 10) minutes = "0" + minutes;
+
+        timerElement.textContent = `${minutes}:${seconds}`;
+
+        if (timeLeft > 0) {
+            timeLeft--;
+            setTimeout(updateTimer, 1000);
+        } else {
+            timerElement.textContent = "00:00";
+        }
+    };
+
+    updateTimer();
+};
+
+startCountdown();
+
+
+
