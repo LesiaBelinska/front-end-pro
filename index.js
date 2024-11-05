dd0a8d3125b499a4cce1616bfed9c59136598137
Lesia Belinska <lesya.belinskaya@appflame.com>
2024-11-05 09:46:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 "use strict";
+
+const API_KEY = "c4ff3dfcad5185a5e167f674538d26d3";
+
+const url =
+  `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${API_KEY}`;
+
+fetch(
+  url
+)
+  .then((response) => response.json())
+
