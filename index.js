4c4dc291ca079b1c80ef84cf1ed0ed1bac06a24f
Lesia Belinska <lesya.belinskaya@appflame.com>
2024-11-06 10:39:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,64 @@
 "use strict";
 
+const API_URL = "https://api.openweathermap.org/data/2.5/weather?q=Kyiv,ua&units=metric&APPID=";
 const API_KEY = "c4ff3dfcad5185a5e167f674538d26d3";
 
-const url =
-  `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${API_KEY}`;
+const updateDateTime = () => {
+  const now = new Date();
 
-fetch(
-  url
-)
-  .then((response) => response.json())
+  const date = now.toLocaleDateString("en-US", {
+    weekday: "short",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
 
+  const time = now.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
+  document.querySelector("#date").textContent = date;
+  document.querySelector("#time").textContent = time;
+}
+
+const fetchWeatherData = () => {
+  fetch(API_URL + API_KEY)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error("Network response was not ok.");
+    })
+    .then((data) => {
+      const cityElement = document.querySelector("#city");
+      const temperatureElement = document.querySelector("#temperature");
+      const descriptionElement = document.querySelector("#description");
+      const humidityElement = document.querySelector("#humidity");
+      const feelsLikeElement = document.querySelector("#feels-like");
+      const pressureElement = document.querySelector("#pressure");
+      const windElement = document.querySelector("#wind");
+  
+
+      cityElement.textContent = data.name;
+      temperatureElement.textContent = `${data.main.temp}°C`;
+      feelsLikeElement.textContent = `Feels like: ${data.main.feels_like}°C`;
+      descriptionElement.textContent = `${data.weather[0].description}`;
+      humidityElement.textContent = `Humidity: ${data.main.humidity}%`;
+      pressureElement.textContent = `Pressure: ${data.main.pressure} hPa`;
+      windElement.textContent = `Wind: ${data.wind.speed} km/h`;
+
+      updateDateTime();
+    })
+    .catch((error) => {
+      console.error("Fetch error:", error);
+    });
+}
+
+fetchWeatherData();
+
+setInterval(updateDateTime, 60000);
+
+const updateButtonElement = document.querySelector("#update-button");
+
+updateButtonElement.addEventListener("click", fetchWeatherData);
