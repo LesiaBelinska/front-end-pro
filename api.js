b54e3ce6bf087699abaf293cb16b19f738a16152
Lesia Belinska <lesya.belinskaya@appflame.com>
2024-11-15 10:41:36 +0200

    Create api.js

---
+++ api.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const API_URL = "https://jsonplaceholder.typicode.com/";
+
+export const fetchPosts = async () => {
+    const response = await fetch(API_URL + "posts?_limit=10");
+
+    if (!response.ok) {
+        throw new Error("Network response was not ok.");
+    }
+
+    return await response.json();
+};
+
+export const fetchPostComments = async (postId) => {
+    const response = await fetch(API_URL + `posts/${postId}/comments?_limit=2`);
+
+    if (!response.ok) {
+        throw new Error("Network response was not ok.");
+    }
+
+    return await response.json();
+};
+
+export const createPost = async (postData) => {
+
+    const response = await fetch(API_URL + "posts", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(postData),
+    });
+
+    if (!response.ok) {
+        throw new Error("Network response was not ok.");
+    }
+    
+    return await response.json();
+};
