b6f3c1b2daca086b9c1694c3ebaf34a155cb9e9c
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-11-19 22:08:49 +0200

    add webpack logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import { showName } from "./js/script.js";
 
 showName('Lesia');
+
+
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,64 @@
-const path = require('path');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  entry: {
-    main: [
-      path.join(__dirname, "./src/index.js"),
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+
+  return {
+    mode: isProduction ? "production" : "development",
+    entry: {
+      main: [
+        path.join(__dirname, "./src/index.js"),
+        path.join(__dirname, "./src/sass/main.scss"),
+      ],
+    },
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js",
+      clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                [
+                  "@babel/preset-env",
+                  {
+                    targets: "> 0.25%",
+                  },
+                ],
+              ],
+            },
+          },
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "src/index.html"), // Використовуйте шаблон
+        filename: "index.html", 
+      }),
+      new MiniCssExtractPlugin({
+        filename: "styles.css",
+      }),
     ],
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
+    devServer: {
+      static: path.resolve(__dirname, "dist"),
+      port: 8080,
+      open: true,
+      devMiddleware: {
+        writeToDisk: false,
+      },
+    },
+  };
 };
